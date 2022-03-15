c4b5e8f5e4c14e67202ca63d95250ded72a9b1e1
Javier Lopez <potnox@gmail.com>
2022-03-15 19:47:42 +0100

    ADD external dependencies

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export { App, Router, Request, Response } from "https://deno.land/x/attain/mod.ts"
+export * as B64 from "https://deno.land/std/encoding/base64url.ts";
+export * as Hash from "https://deno.land/std/hash/sha256.ts";
+export * as Bcrypt from "https://deno.land/x/bcrypt/mod.ts";
