0ca67786b24f2f9ae0b456bf7bfa8ab95a62ec5b
Javier Lopez <potnox@gmail.com>
2022-03-19 10:28:57 +0100

    ADD new use case to allow a user to get all their information

--- usecases/UserPostUseCases.ts
+++ usecases/UserPostUseCases.ts
@@ -13,3 +13,26 @@ export function usecaseUserDeleteAccount(user_id:string, userRepository:iUserRep
     usecaseDeleteAllOwnerPosts(user_id,postRepository)
     userRepository.deleteUser(user_id)
 }
+
+/*
+    USE CASE: As a user I want to be able to get all my content
+    owner_id: string
+    UserRepository: iUserRepository
+    PostRepository: iPostRepository
+*/
+export function usecaseGetAllInfoFromUser(user_id:string, userRepository:iUserRepository, postRepository:iPostRepository):any
+{
+    const user = userRepository.getUserById(user_id)
+    if(user == null) return {}
+    const posts = postRepository.getPostsByOwner(user_id)
+    return {
+        user:{
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt
+        },
+        posts
+    }
+}
